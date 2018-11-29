781f5b7d6d1de67babe7f1290ccf39eefaf805b4
Josh Campbell <josh@userdel.com>
2018-11-29 09:38:02 -0800

    makes the api more clear and moves version, author, and license in

--- pydng/__init__.py
+++ pydng/__init__.py
@@ -1,1 +1,11 @@
-from pydng.pydng import generate_name
+from .pydng import generate_name
+
+__author__ = "Josh Campbell"
+__license__ = "MIT"
+__version__ = "1.1.1"
+
+# API
+__all__ = [
+    "__version__",
+    "generate_name",
+]
